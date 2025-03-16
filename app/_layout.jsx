a1190198d59627a7f977564542dda6d306b89b96
Ismael Borquez <ysmaelborquezz@gmail.com>
2025-03-16 02:11:14 -0300

    fix: remove about header button

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,8 +1,7 @@
-import { Link, Stack } from "expo-router";
-import { Pressable, View } from "react-native";
+import { Stack } from "expo-router";
+import { View } from "react-native";
 import "../styles/global.css";
 import LogoIcon from "../components/Icons/LogoIcon";
-import { CircleInfoIcon } from "../components/Icons/Icons";
 
 export default function Layout() {
   return (
@@ -13,13 +12,6 @@ export default function Layout() {
           headerTintColor: "white",
           headerTitle: "",
           headerLeft: () => <LogoIcon />,
-          headerRight: () => (
-            <Link href="/about" style={{ color: "white" }} asChild>
-              <Pressable className="active:opacity-50">
-                <CircleInfoIcon />
-              </Pressable>
-            </Link>
-          ),
         }}
       />
     </View>
